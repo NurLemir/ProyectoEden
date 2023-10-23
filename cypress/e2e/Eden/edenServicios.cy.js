552481f1f4ec9945053cbc17fb6d542fc6b05d87
NurLemir <nurlemir17@gmail.com>
2023-10-23 12:25:57 -0300

    Se agregó test de servicio

--- cypress/e2e/Eden/edenServicios.cy.js
+++ cypress/e2e/Eden/edenServicios.cy.js
@@ -4,7 +4,19 @@ describe("TEST DE SERVICIO DE EDEN", () => {
     it("Verificar Servicio de INICIO", () => {
         cy.request("GET", "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio",
             ).then((response) => {
-            expect(response.status).to.eq(200);
+                cy.log(`Respuesta del servicio de Inicio: ${JSON.stringify(response)}`);
+            expect(response.status).to.eq(200);   
         });
     });
-});
+
+    it("Verificar Servicio de INICIO 2", () => {
+        cy.request( {
+            method: "GET", 
+            url: "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio",
+        }).then((response) => {
+                cy.log(`Respuesta del servicio de Inicio: ${JSON.stringify(response)}`);
+            expect(response.status).to.eq(200);   
+            cy.writeFile("cypress/fixtures/autogenerado/eventos.json", response["body"]);
+        });
+    });
+})
