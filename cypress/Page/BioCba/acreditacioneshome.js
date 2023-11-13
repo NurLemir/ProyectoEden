fcdd91a567f8b08649aa03442ffd45df0b549a5d
NurLemir <nurlemir17@gmail.com>
2023-11-13 12:54:43 -0300

    Se agregaron test visuales

--- cypress/Page/BioCba/acreditacioneshome.js
+++ cypress/Page/BioCba/acreditacioneshome.js
@@ -2,6 +2,10 @@ class AcreditacionesHome {
     getMenuButton() {
         return cy.get("a");
     }
+
+    getImgFlamingo() {
+        return cy.get("[data-url]");
+    }
 }
 
 export default new AcreditacionesHome();
--- cypress/e2e/BioCba/acreditacionesHome.cy.js
+++ cypress/e2e/BioCba/acreditacionesHome.cy.js
@@ -9,10 +9,24 @@ describe ("Tests sobre Acreditaciones del Parque de la Biodiversidad", () => {
     
     it("Funcionamiento de Botones de Home", () => {
 
-        acreditacionesHome.getMenuButton().contains("ESPAÑOL").should("have.attr", "onclick");
-        acreditacionesHome.getMenuButton().contains("ENGLISH").should("have.attr", "onclick");
-        acreditacionesHome.getMenuButton().contains("ANULAR MI INSCRIPCIÓN").should("have.attr", "href");
+        acreditacionesHome.getMenuButton().contains("ESPAÑOL")
+        .should("have.attr", "onclick");
+        acreditacionesHome.getMenuButton().contains("ENGLISH")
+        .should("have.attr", "onclick");
+        acreditacionesHome.getMenuButton().contains("ANULAR MI INSCRIPCIÓN")
+        .should("have.attr", 
+        "href", 
+        "https://acreditaciones.biocordoba.com.ar/anular-inscripcion");
 
+        acreditacionesHome.getMenuButton()
+        .should("have.css", 
+        "Background", 
+        "rgb(254, 250, 224) none repeat scroll 0% 0% / auto padding-box border-box");
+
+
+        it("Verificar Imagen de Flamencos", () => {
+            acreditacionesHome.getImgFlamingo().should("have.attr", "src", "url(https://acreditaciones.biocordoba.com.ar/assets/images/bkg.webp) no-repeat center center/cover;");
+        });
     });
 });
 
